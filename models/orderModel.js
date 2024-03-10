d301e4ce72dad122b4afde2c820c7ecd77379992
Volodymyr Peretiatko <djbob2000@gmail.com>
2024-03-10 10:15:57 +0200

    fix: order model

--- models/orderModel.js
+++ models/orderModel.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const OrderSchema = new Schema({
-  order: [
+  cartGoods: [
     {
       shopId: String,
       title: String,
